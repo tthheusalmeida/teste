71ef11697756a4eb96423a81ae4f219164db6458
Matheus Almeida <43671640+tthheusalmeida@users.noreply.github.com>
2025-01-22 10:14:22 -0300

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+h1 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
